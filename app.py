eef859269112243bead070e72dc32afe081da4ce
Dipta <dipta@conn3cted.com>
2024-08-20 19:29:15 +0600

    Updated project with recent changes

--- app.py
+++ app.py
@@ -1,69 +1,55 @@
 import streamlit as st
 import pickle
 import nltk
+from nltk import word_tokenize
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
 
 # Download NLTK resource
 nltk.download('punkt')
+nltk.download('stopwords')
 
 porter_stemmer = PorterStemmer()
 
-def modify_text(text):
-    # lower casing
+# Define text preprocessing function
+def preprocess_text(text):
+    # Convert text to lowercase
     text = text.lower()
+    # Remove punctuation
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Tokenize text bhjnu
+    tokens = word_tokenize(text)
+    # Remove stop words
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words]
+    # Stemming
+    stemmer = PorterStemmer()
+    tokens = [stemmer.stem(word) for word in tokens]
+    # Join tokens back into text
+    preprocessed_text = ' '.join(tokens)
+    return preprocessed_text
 
-    # tokenization
-    text = nltk.word_tokenize(text)
-
-    # special charecter removal
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    # stop words/punctuations removal
-    text = y[:]  # cloning to reassign text
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    # stemming
-    text = y[:]  # cloning to reassign text
-    y.clear()
-
-    for i in text:
-        y.append(porter_stemmer.stem(i))
-
-    return " ".join(y)
-
-
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
+model = pickle.load(open('naive_bayes_model.pkl', 'rb'))
 
 st.title("Spam Email Classifier")
 
-#accept user input
-input = st.text_area("Enter the email you want to test")
+# Accept user input
+input = st.text_area("Enter the text you want to test")
 
 if st.button('Predict'):
+    # Preprocess user input
+    preprocessed_text = preprocess_text(input)
 
-    #Preprocess
-    modified_email = modify_text(input)
+    # Vectorize the preprocessed text
+    vectorized_input = vectorizer.transform([preprocessed_text])
 
-    #Vectorize
-    vectorized_input = tfidf.transform([modified_email])
-
-    #predict
+    # Predict the result
     result = model.predict(vectorized_input)[0]
 
-    #Output
+    # Display the result
     if result == 1:
         st.header("Spam :(")
     else:
         st.header("Ham :)")
-
-
